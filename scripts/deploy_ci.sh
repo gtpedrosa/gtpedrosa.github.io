3043ce0f2b4217db057a8f9619356b9245e05ff7
Guilherme Pedrosa <guilherme.pedrosa@gmail.com>
2021-04-25 14:18:54 -0300

    helper msgs to debug deployment

--- scripts/deploy_ci.sh
+++ scripts/deploy_ci.sh
@@ -10,10 +10,14 @@ cp README.md master/
 
 echo "* pushing to master:"
 cd master
+echo "cd OK"
 git config user.name "CircleCI"
 git config user.email ${GIT_EMAIL}
+echo "git config OK"
 git add -A
+echo "git add OK"
 git commit -m "Automated deployment job ${CIRCLE_BRANCH} #${CIRCLE_BUILD_NUM} [skip ci]" --allow-empty
+echo "git commit OK"
 git push origin master
 
 echo "* done"
